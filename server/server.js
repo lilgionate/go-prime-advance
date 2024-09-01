429d91c790541ea0bf79287aa56e67938cf947aa
lilgionate <140750280+lilgionate@users.noreply.github.com>
2024-08-31 21:47:10 -0400

    Fix deploy issues

--- server/server.js
+++ server/server.js
@@ -1,9 +1,13 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, '..', 'client', 'build')));
+
 app.get('/', (req, res) => {
-  res.send('Hello, world!');
+  res.sendFile(path.join(__dirname, '..', 'client', 'build', 'index.html'));
 });
 
 app.listen(PORT, () => {
